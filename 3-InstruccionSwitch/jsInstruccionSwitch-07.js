3e8805738b19ba2d7a70e9be4881c69deee59970
tomasperezponisio <t.perezponisio@gmail.com>
2021-07-09 17:38:06 -0300

    ejercicios switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,31 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	var destinoIngresado;
+	var mensaje;
+
+	destinoIngresado = document.getElementById('txtIdDestino').value;
+
+	switch(destinoIngresado) {
+		case "Bariloche" : {
+			mensaje = "Oeste";
+			break;
+		}		
+		case "Cataratas" : {
+			mensaje = "Norte";			
+			break;
+		}
+		case "Mar del plata" : {
+			mensaje = "Este";		
+			break;
+		}		
+		case "Ushuaia" : {
+			mensaje = "Sur";		
+			break;
+		}
+		default: {
+			mensaje = "Seleccione un destino";
+		}
+	}
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,24 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	var destinoIngresado;
+	var mensaje;
+
+	destinoIngresado = document.getElementById('txtIdDestino').value;
+
+	switch(destinoIngresado) {
+		case "Bariloche" :
+		case "Mar del plata" :
+		case "Ushuaia" : {
+			mensaje = "Frío";		
+			break;
+		}case "Cataratas" : {
+			mensaje = "Calor";			
+			break;
+		}
+		default: {
+			mensaje = "Seleccione un destino";
+		}
+	}
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
