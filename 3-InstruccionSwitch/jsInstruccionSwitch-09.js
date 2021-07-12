84cb1bc0561b61e261049bdc90437043b4e604de
tomasperezponisio <t.perezponisio@gmail.com>
2021-07-12 18:40:44 -0300

    ejercicios

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,32 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	var destinoIngresado;
+	var estacionIngresada;
+	var mensaje;
+	var tarifa;
+	var aumento;
+	var descuento;
+
+	destinoIngresado = document.getElementById('txtIdDestino').value;
+	estacionIngresada = document.getElementById('txtIdEstacion').value;
+	tarifa = 15000;
+	aumento = 0;
+	descuento = 0;
+
+	switch(destinoIngresado) {
+		case "Bariloche" :
+		case "Mar del plata" :
+		case "Ushuaia" : {
+			mensaje = "Frío";		
+			break;
+		}case "Cataratas" : {
+			mensaje = "Calor";			
+			break;
+		}
+		default: {
+			mensaje = "Seleccione un destino";
+		}
+	}
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
