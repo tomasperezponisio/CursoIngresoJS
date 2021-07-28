d16a30e81cf61ab47be0ddeb4809b32640b92901
tomasperezponisio <t.perezponisio@gmail.com>
2021-07-28 12:29:19 -0300

    parciales

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,4 +1,167 @@
+/*
+autor: Tomas Perez Ponisio
+ejercicio: Parcial 2020 Ejercicio 2 
+
+Realizar el algoritmo que permita ingresar los datos de una compra productos de la construccion,
+hasta que el cliente quiera:
+Tipo validad("arena";"cal";"cemento")
+Cantidad de bolsas,
+Precio por bolsa (más de cero ),
+
+Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
+Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
+a) El importe total a pagar , bruto sin descuento y...
+b) el importe total a pagar con descuento(solo si corresponde)
+d) Informar el tipo con mas cantidad de bolsas.
+f) El tipo mas caro
+*/
+
 function mostrar()
 {
-  alert("dos");
+  var producto;
+  var cantidad;
+  var precio;
+  var seguir;
+  var precioPrevio;
+  var precioTotalArena;
+  var precioTotalCal;
+  var precioTotalCemento;
+  var cantidadTotalArena;
+  var cantidadTotalCal;
+  var cantidadTotalCemento;
+  var cantidadTotalTodos;
+  var precioTotalSinDescuento;
+  var precioFinalConDescuento;
+  var descuentoPorcentaje;
+  var descuento;
+  var cantidadMaxima;
+  var banderaCantidadMaxima;
+  var productoConMasCantidad;
+  var productoMasCaro;
+  var precioMaximo;
+  var banderaPrecioMaximo;
+  
+  seguir = true;
+  precioTotalArena = 0;
+  precioTotalCal = 0;
+  precioTotalCemento = 0;
+  cantidadTotalArena = 0;
+  cantidadTotalCal = 0;
+  cantidadTotalCemento = 0;
+  cantidadTotalTodos = 0;
+  banderaCantidadMaxima = true;
+  banderaPrecioMaximo = true;
+
+  while (seguir == true)
+  {
+    producto = prompt("Qué producto quiere comprar?");
+    while (producto != "arena" && producto != "cal" && producto != "cemento")
+    {
+      producto = prompt("Qué producto quiere comprar? (arena / cal / cemento)");
+    }
+
+    cantidad = prompt("Cuantas bolsas quiere comprar?");
+    cantidad = parseInt(cantidad);
+    while (cantidad < 1)
+    {
+      cantidad = prompt("Cuantas bolsas quiere comprar? Ingrese una cantidad valida");
+      cantidad = parseInt(cantidad);
+    }
+
+    precio = prompt("Cuál es el precio por bolsa?");
+    precio = parseInt(precio);
+    while (precio < 0)
+    {
+      precio = prompt("Cuál es el precio por bolsa? Ingrese un precio mayor a 0");
+      precio = parseInt(precio);
+    }
+
+    switch (producto)
+    {
+      case "arena":
+        precioPrevio = cantidad * precio;
+        precioTotalArena = precioTotalArena + precioPrevio;
+        cantidadTotalArena = cantidadTotalArena + cantidad;
+        break;
+      
+      case "cal":
+        precioPrevio = cantidad * precio;
+        precioTotalCal = precioTotalCal + precioPrevio;
+        cantidadTotalCal = cantidadTotalCal + cantidad;
+        break;
+
+      case "cemento":
+        precioPrevio = cantidad * precio;
+        precioTotalCemento = precioTotalCemento + precioPrevio;
+        cantidadTotalCemento = cantidadTotalCemento + cantidad;
+        break;
+    }
+
+    if (banderaCantidadMaxima == true)
+    {
+      cantidadMaxima = cantidad;
+      productoConMasCantidad = producto;
+      banderaCantidadMaxima = false;
+    }
+    if (cantidad > cantidadMaxima)
+    {
+      cantidadMaxima = cantidad;
+      productoConMasCantidad = producto;
+    }
+
+    if (banderaPrecioMaximo == true)
+    {
+      precioMaximo = precio;
+      productoMasCaro = producto;
+      banderaPrecioMaximo = false;
+    }
+    if (precio > precioMaximo)
+    {
+      precioMaximo = precio;
+      productoMasCaro = producto;
+    }
+
+    seguir = confirm("Desea ingresar otro producto?");
+  }
+
+  //Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
+  //Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
+  
+  precioTotalSinDescuento = precioTotalArena + precioTotalCal + precioTotalCemento;
+  cantidadTotalTodos = cantidadTotalArena + cantidadTotalCal + cantidadTotalCemento;
+
+  if (cantidadTotalTodos > 10 && cantidadTotalTodos < 31)
+  {
+    descuentoPorcentaje = 15;
+  } 
+  else
+  {
+    if (cantidadTotalTodos > 30)
+    {
+      descuentoPorcentaje = 25;
+    }
+    else
+    {
+      descuentoPorcentaje = 0;
+    }
+  }
+  // a) El importe total a pagar , bruto sin descuento y...
+  // b) el importe total a pagar con descuento(solo si corresponde)
+  // d) Informar el tipo con mas cantidad de bolsas.
+  // f) El tipo mas caro
+
+  descuento = precioTotalSinDescuento * descuentoPorcentaje / 100;
+  precioFinalConDescuento = precioTotalSinDescuento - descuento;
+
+  //document.write("cantidadTotalArena: " + cantidadTotalArena + " || cantidadTotalCal: " + cantidadTotalCal + " || cantidadTotalCemento: " + cantidadTotalCemento);
+  //document.write("<br>");
+  //document.write("precioTotalArena: " + precioTotalArena + " || precioTotalCal: " + precioTotalCal + " || precioTotalCemento: " + precioTotalCemento);
+  //document.write("<br>");
+  document.write("Importe total bruto: " + precioTotalSinDescuento + " || descuento del (" + descuentoPorcentaje +  "%): " + descuento + " || precio final con descuento: " + precioFinalConDescuento);
+  document.write("<br>");
+  document.write("El producto con más cantidad fue: " + productoConMasCantidad + " con " + cantidadMaxima + " bolsas");
+  document.write("<br>");
+  document.write("El producto más caro fue: " + productoMasCaro + " y costó " + precioMaximo);
+  
+  
 }
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
@@ -1,4 +1,124 @@
+/*
+autor: Tomas Perez Ponisio
+ejercicio: Parcial 2020 Ejercicio 3
+
+En el ingreso a un viaje en avion nos solicitan nombre , edad,
+sexo("f" o "m") y estado civil("soltero", "casado" o "viudo")y temperatura corporal.
+a) El nombre de la persona con mas temperatura.
+b) Cuantos mayores de edad estan viudos
+c) La cantidad de hombres que hay solteros o viudos.
+d) cuantas personas de la tercera edad( mas de 60 años) , tienen mas de 38 de temperatura
+e) El promedio de edad entre los hombres solteros.
+*/
+
 function mostrar()
 {
-	alert("tres");
+	var nombre;
+	var edad;
+	var sexo;
+	var estadoCivil;
+	var temperatura;
+	var seguir;
+	var temperaturaMaxima;
+	var nombreTemperaturaMaxima;
+	var banderaTemperaturaMaxima;
+	var contadorMayoresDeEdadViudos;
+	var cantidadHombresSolterosOViudos;
+	var cantidadTerceraEdadConFiebre;
+	var cantidadSolteros;
+	var sumaEdadSolteros;
+	var promedioEdadHombresSolteros;
+
+	seguir = true;
+	banderaTemperaturaMaxima = true;
+	contadorMayoresDeEdadViudos = 0;
+	cantidadHombresSolterosOViudos = 0;
+	cantidadTerceraEdadConFiebre = 0;
+	promedioEdadHombresSolteros = 0;
+	cantidadSolteros = 0;
+	sumaEdadSolteros = 0;
+	promedioEdadHombresSolteros = 0;
+
+	while(seguir == true)
+	{
+		nombre = prompt("Ingrese nombre de pasajero");
+		edad = prompt("Ingrese la edad");
+		edad = parseInt(edad);
+		while(edad < 1 || edad > 110)
+		{
+			edad = prompt("Ingrese una edad valida, 1-110");
+			edad = parseInt(edad);
+		}
+		sexo = prompt("Ingrese el sexo");
+		while (sexo != "f" && sexo != "m")
+		{
+			sexo = prompt("Ingrese el sexo correcto, f/m");
+		}
+		estadoCivil = prompt("Ingrese estado civil");
+		while (estadoCivil != "soltero" && estadoCivil != "casado" && estadoCivil != "viudo")
+		{
+			estadoCivil = prompt("Ingrese estado civil correcto, soltero/casado/viudo");
+		}
+		temperatura = prompt("Ingrese la temperatura");
+		temperatura = parseInt(temperatura);
+		while (temperatura < 35 || temperatura > 45)
+		{
+			temperatura = prompt("Ingrese la temperatura correcta, 35-45");
+			temperatura = parseInt(temperatura);
+		}
+
+		if (banderaTemperaturaMaxima == true)
+		{
+			temperaturaMaxima = temperatura;
+			nombreTemperaturaMaxima = nombre;
+			banderaTemperaturaMaxima = false;
+		}
+		if (temperatura > temperaturaMaxima)
+		{
+			nombreTemperaturaMaxima = nombre;
+			temperaturaMaxima = temperatura;
+		}
+
+		if (edad > 18 && estadoCivil == "viudo")
+		{
+			contadorMayoresDeEdadViudos++;
+		}
+
+		if (sexo == "m")
+		{
+			if(estadoCivil == "soltero" || estadoCivil == "viudo")
+			{
+				cantidadHombresSolterosOViudos++;
+			}
+		}
+
+		if (edad > 60)
+		{
+			if (temperatura > 38)			
+			{
+				cantidadTerceraEdadConFiebre++;
+			}
+		}
+
+		if (estadoCivil == "soltero")
+		{
+			cantidadSolteros++;
+			sumaEdadSolteros = sumaEdadSolteros + edad;
+		}
+
+		seguir = confirm("Desea agregar otro pasajero?");
+	}
+
+	promedioEdadHombresSolteros = sumaEdadSolteros / cantidadSolteros ;
+
+	document.write("El nombre de la persona con mayor temperatura es: " + nombreTemperaturaMaxima + " y tuvo " + temperaturaMaxima +  " grados");
+  	document.write("<br>");
+	document.write("Cantidad de mayores de edad viudos: " + contadorMayoresDeEdadViudos);
+  	document.write("<br>");
+	document.write("Cantidad de hombres solteros o viudos: " + cantidadHombresSolterosOViudos);
+  	document.write("<br>");
+	document.write("Cantidad de mayores de 60 años con mas de 38 grados: " + cantidadTerceraEdadConFiebre);
+  	document.write("<br>");
+	document.write("Promedio de edad de los solteros: " + promedioEdadHombresSolteros);
+  	document.write("<br>");
 }
